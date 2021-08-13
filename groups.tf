38bdb3c162b4294444726d0b9db8dbaa4ba90300
Tu Nguyen <im2nguyen@gmail.com>
2021-08-13 02:15:34 -0700

    Update provider version

--- groups.tf
+++ groups.tf
@@ -33,3 +33,15 @@ resource "azuread_group_member" "engineers" {
   group_object_id  = azuread_group.engineers.id
   member_object_id = each.value.id
 }
+
+resource "azuread_group" "customer_success" {
+  display_name = "Education - Customer Success"
+  security_enabled = true
+}
+
+resource "azuread_group_member" "customer_success" {
+  for_each = { for u in azuread_user.users: u.mail_nickname => u if u.job_title == "Customer Success" }
+
+  group_object_id  = azuread_group.customer_success.id
+  member_object_id = each.value.id
+}
--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azuread = {
-      source  = "hashicorp.com/edu/azuread"
+      source  = "hashicorp/azuread"
       version = "~> 2.0.0"
     }
   }
